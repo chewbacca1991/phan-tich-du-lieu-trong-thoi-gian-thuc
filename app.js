b7ecdceef8eee457bfd46288a576b253f1c1b744
chewbacca1991 <mounafbanbo@hotmail.com>
2025-09-25 18:25:23 +0330

    feat: initial commit for app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const { fetchData } = require('./dataProcessor');
+const { generateReport } = require('./reportGenerator');
+const { checkAndAlert } = require('./alerts');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/get-data', async (req, res) => {
+    const data = await fetchData('http://api.example.com/data');
+    if (data) {
+        const report = generateReport(data);
+        checkAndAlert(data, 100); // Giả định 100 là ngưỡng báo động
+        res.send(report);
+    } else {
+        res.status(500).send('Không thể lấy dữ liệu.');
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server đang chạy trên http://localhost:${PORT}`);
+});
